1543aefab0429784db90bf65e07ec5d142b10f91
Razu <shahnaouzrazu21@gmail.com>
2018-08-03 02:32:44 +0600

    Review Created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     //
-    
+    protected $fillable = [
+      'customer','review','star'  
+    ];
     public function products(){
         return $this->belongsTo(Product::class);
     }
